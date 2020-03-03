049d13d5ec6834a2dd073198c16fb3f0784f9972
Carlos M. Cabrera <243419+pointtonull@users.noreply.github.com>
2020-03-03 13:48:31 +0000

    common window mistake

--- __init__.py
+++ __init__.py
@@ -14,6 +14,7 @@ def get_editor():
     user_choice = config.get("editor")
     editors = [
         user_choice,
+        user_choice + ".exe",
         "notepad++.exe",
         "notepad.exe",
         "gvim",
